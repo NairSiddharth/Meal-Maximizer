db108e4399e29a1aaecb4755a054dad68377258f
NairSiddharth <nair.sid01@gmail.com>
2020-12-22 15:06:43 -0600

    Add files via upload
    A minor update cleaning the app folders of extraneous files that have been subsumed by other, currently in use files. Also adjusts certain parts of two layouts for ease of use. Lastly changes the package name, versioncode/versionname, and build.Gradle file in preparation for the release of this app.

---
+++ App Builds/AndroidApp(12_19)Complete/app/src/main/java/com/example/release/Custom.java
@@ -0,0 +1,97 @@
+package com.example.release;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Custom extends AppCompatActivity {
+    double diningdollars;
+    int mealswipes;
+    String startdate;
+    String enddate;
+    int totaldays;
+    EditText diningInput;
+    EditText mealInput;
+    EditText startInput;
+    EditText endInput;
+    Button submitButton;
+    public static int days(String start, String end){
+
+        int[] daysinm = {31,28,31,30,31,30,31,31,30,31,30,31};
+        String[] startdate = start.split("/",2);
+        String[] enddate = end.split("/",2);
+
+        int startmonth = Integer.parseInt(startdate[0]);
+        int endmonth = Integer.parseInt(enddate[0]);
+        if(startmonth >12 ||endmonth >12){
+            System.out.print("error enter in a month less than or equal to 12");
+            return 0;
+        }
+        if(startmonth>endmonth && endmonth<6) {
+            endmonth = endmonth+12;
+        }
+        int startday = Integer.parseInt(startdate[1]);
+        int endday = Integer.parseInt(enddate[1]);
+        int totaldays = 0;
+        for(int i = startmonth; i<endmonth; i++){
+            totaldays+=daysinm[i%12];
+        }
+        totaldays = totaldays-startday+endday;
+        return totaldays;
+    }
+    public static double[] calculations(int totaldays, double mealswipes, double diningdollars) {
+        double mpd = mealswipes/totaldays;
+        double ddpd = diningdollars/totaldays;
+        double mpw = mpd * 7;
+        double ddpw = ddpd * 7;
+        return(new double[]{mpd,mpw,ddpd,ddpw});
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+        diningInput = (EditText) findViewById(R.id.idDineDollars);
+        mealInput = (EditText) findViewById(R.id.idMealSwipes);
+        startInput = (EditText) findViewById(R.id.idStartDate);
+        endInput = (EditText) findViewById(R.id.idEndDate);
+        submitButton = (Button) findViewById(R.id.idsubmitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                InputMethodManager inputManager = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+
+
+                diningdollars = Double.parseDouble(diningInput.getText().toString());
+                mealswipes = Integer.valueOf(mealInput.getText().toString());
+                startdate = startInput.getText().toString();
+                enddate = endInput.getText().toString();
+                totaldays = days(startdate, enddate);
+                double[] finalvalue = calculations(totaldays,  mealswipes,diningdollars);
+                double mpd = finalvalue[0];
+                double mpw = finalvalue[1];
+                if (!(mpd < 1)) {
+                    mpd = Math.round(mpd);
+                    mpw = Math.round(mpw);
+                }
+                double  ddpd= finalvalue[2];
+                double ddpw = finalvalue[3];
+                TextView mmpdtext = (TextView) findViewById(R.id.mmpd);
+                TextView mmpwtext = (TextView) findViewById(R.id.mmpw);
+                TextView ddpdtext = (TextView) findViewById(R.id.ddpd);
+                TextView ddpwtext = (TextView) findViewById(R.id.ddpw);
+                mmpdtext.setText(String.format("%.2f",ddpd)+" Dining Dollars Per Day");
+                mmpwtext.setText(String.format("%.2f",ddpw)+ " Dining Dollars Per Week " );
+                ddpdtext.setText(String.format("%.2f",mpd)+" Meal Swipes Per Day");
+                ddpwtext.setText(String.format("%.2f",mpw)+" Meal Swipes Per Week");
+            }
+        });
+    }
+}
---
+++ App Builds/AndroidApp(12_19)Complete/app/src/main/java/com/example/release/presets.java
@@ -0,0 +1,76 @@
+package com.example.release;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class presets extends AppCompatActivity {
+    public static final java.lang.String EXTRA_BOOL = "com.example.application.example.EXTRA_BOOL";
+    private boolean Sem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presets);
+        final TextView semdis = (TextView)findViewById(R.id.textView26) ;
+
+        SwitchCompat Semester = (SwitchCompat) findViewById(R.id.switch1);
+        Semester.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    Sem = true;
+                    semdis.setText("Spring Semester");
+                }
+                else{
+                    Sem = false;
+                    semdis.setText("Fall Semester");
+                }
+            }
+        });
+        Button freshman = findViewById(R.id.freshman);
+        freshman.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openFreshman();
+            }
+        });
+        Button upper = findViewById(R.id.button6);
+        upper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUpper();
+            }
+        });
+        Button corp = findViewById(R.id.button5);
+        corp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCorp();
+            }
+        });
+    }
+    public void openFreshman() {
+        Intent intent = new Intent(this, freshman.class);
+        intent.putExtra(EXTRA_BOOL,Sem);
+        startActivity(intent);
+    }
+    public void openUpper() {
+        Intent intent = new Intent(this, Upper.class);
+        intent.putExtra(EXTRA_BOOL,Sem);
+        startActivity(intent);
+    }
+    public void openCorp() {
+        Intent intent = new Intent(this, Corp.class);
+        intent.putExtra(EXTRA_BOOL,Sem);
+        startActivity(intent);
+    }
+}
