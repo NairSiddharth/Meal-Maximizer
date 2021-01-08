fc6675baa75dffd6831771284d406e514957a323
Chris Pasala <chrispasala@tamu.edu>
2021-01-08 11:19:31 -0600

    Added Buttons of Preset Custom Freshman and Corp pages

---
+++ Backend Versions/ButtonsV1/freshman.swift
@@ -0,0 +1,83 @@
+import Foundation
+
+Button(action: {
+    preset(100,300)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(130,300)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(165,300)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(200,300)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(240,300)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(100,400)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(130,400)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(165,400)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(200,400)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(240,400)
+}) {
+    // How the button looks like
+}
+
+Button(action: {
+    preset(100,500)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(130,500)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(165,500)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(200,500)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(240,500)
+}) {
+    // How the button looks like
+}
+Button(action: {
+    preset(0,1350)
+}) {
+    // How the button looks like
+}
